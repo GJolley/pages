859cf9eb8e1250caf1953c2b63adb84966bc08f6
Garrett <garrett.jolley@gmail.com>
2022-08-28 22:35:20 -0500

    initial commit

---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+from urllib.parse import urlparse
+from django.urls import path
+from .views import HomePageView, AboutPageView
+
+
+urlpatterns = [
+    path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),
+]
